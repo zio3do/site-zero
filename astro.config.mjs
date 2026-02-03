ef2caba3608a7c90ecc87dd39a57861f3e3ab0e0
Zion Hadley <zionhadley@Sensei-Zs-MBP.localdomain>
2026-02-03 16:21:15 -0500

    updated astro config paths so that css/assets can be found at expected locations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-const site = process.env.SITE_URL;
-
 export default defineConfig({
   output: 'static',
-  site,
+  site: 'https://zio3do.github.io',
 });
